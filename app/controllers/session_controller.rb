830409d33d9f0b807226aa53adae3d2102b50d20
gretchenziegler <gretchenziegler@gmail.com>
2015-04-03 15:10:55 -0400

    additional setup: routes and empty views

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,3 +1,18 @@
 class SessionController < ApplicationController
 
+	# GET '/'
+	def index
+
+	end
+
+	# POST '/session'
+	def create
+
+	end
+
+	# DELETE '/session/:id'
+	def destroy
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'session#index'
+
+  post 'session' => 'session#create'
+
+  delete 'session/:id' => 'session#destroy'
+
+  resources :users do
+    resources :pets
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
